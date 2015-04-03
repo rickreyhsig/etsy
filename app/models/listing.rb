a02ff0d56caf22002869ade86da926be613811e8
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-04-03 18:00:13 +0000

    update code

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ActiveRecord::Base
   if Rails.env.production? # PROD; works with Dropbox
-  has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "default.png",
+    has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "default.png",
     :storage => :dropbox,
     :dropbox_credentials => Rails.root.join("config/dropbox.yml"), 
     :path => ":style/:id_:filename"
